228812bc81a63b07402765fe0a3652e2b1bd3d1b
yannngithub <97786441+yannngithub@users.noreply.github.com>
2022-03-24 02:25:36 +0700

    Update tubemp3-biz.js

--- utils/tubemp3-biz.js
+++ utils/tubemp3-biz.js
@@ -12,7 +12,7 @@ function getAudioWithTubeMp3(link) {
         }
         axios({
             method: "GET",
-            url: `https://api.vevioz.com/api/button/mp3/${VideoID[1]}`,
+            url: `https://api.download-lagu-mp3.com/@api/button/mp3/${VideoID[1]}`,
             headers: {
                 "Content-type": "text/html"
             }
